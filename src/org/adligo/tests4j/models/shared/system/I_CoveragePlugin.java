121247d05f27d157074fd3f1471788bd0eb650e2
scott <scott@adligo.com>
2014-04-19 14:17:04 -0500

    simple jacoco plugin working ok

--- src/org/adligo/tests4j/models/shared/system/I_CoveragePlugin.java
+++ src/org/adligo/tests4j/models/shared/system/I_CoveragePlugin.java
@@ -4,7 +4,13 @@ import java.util.List;
 
 import org.adligo.tests4j.models.shared.I_AbstractTrial;
 
-
+/**
+ * a pluggable interface for a this integrating testing api 
+ * code coverage tools (like EclEmma/Jacoco)
+ * 
+ * @author scott
+ *
+ */
 public interface I_CoveragePlugin {
 	/**
 	 * this instruments the classes so that
@@ -12,5 +18,17 @@ public interface I_CoveragePlugin {
 	 */
 	public List<Class<? extends I_AbstractTrial>> instrumentClasses(I_Tests4J_Params params);
 	
+	/**
+	 * for recording all coverage for a run of trials
+	 * the scope is;
+	 * @see I_CoverageRecorder#TRIAL_RUN
+	 * for coverage recorders that can and want to record 
+	 * at a more detailed level the scope is;
+	 * TrialClassName for capturing the code covered by the trial
+	 * TrialClassName#TestMethodName for capturing the code covered by the @Test
+	 * 
+	 * @param scope
+	 * @return
+	 */
 	public I_CoverageRecorder createRecorder(String scope);
 }
