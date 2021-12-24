84dc2da84b91e13bfb0efd17658cf21e03ea5a48
Scott Morgan <scott@adligo.com>
2021-12-24 16:53:09 -0600

    Working with or without the -Ptag setting

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,14 +20,28 @@ sourceSets {
 
 
 
-val tag : String = project.getProperties().get("tag") as String
+fun getProp(key: String, default: String): String {
+  var r : String = default
+  if (project.hasProperty(key)) {
+    r = project.getProperties().get(key) as String
+  }
+  return r
+}
+
+fun getTag(): String {
+  val current = LocalDateTime.now()
+  val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd_HH-mm-ss")
+  val snapshot = "SNAPSHOT_" + current.format(formatter)
+  //println(snapshot)
+  return getProp("tag", snapshot)
+}
 
 publishing {
   publications {
     create<MavenPublication>("maven") {
       groupId = "org.adligo"
       artifactId = "tests4j"
-      version = tag
+      version = getTat()
       from(components["java"])
     }
   }
