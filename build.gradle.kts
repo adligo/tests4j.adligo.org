ad2f734f3a134a276c13d7b53bd98d71e381bfd4
Scott Morgan <scott@adligo.com>
2021-12-24 16:22:38 -0600

    This now works with commands like; gradle publishToMavenLocal -Ptag=<the current git tag> Cool

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,14 @@
 plugins {
+  `maven-publish`
   eclipse
   java
+  signing
 }
 
 dependencies {
   implementation("javax.xml.bind:jaxb-api:2.4.0-b180830.0359")
 }
+
 
 sourceSets {
   main {
@@ -15,7 +18,27 @@ sourceSets {
   }
 }
 
+
+
+val tag : String = project.getProperties().get("tag") as String
+
+publishing {
+  publications {
+    create<MavenPublication>("maven") {
+      groupId = "org.adligo"
+      artifactId = "tests4j"
+      version = tag
+      from(components["java"])
+    }
+  }
+}
+
+
 repositories {
   mavenLocal()
   mavenCentral()
 }
+
+
+
+
